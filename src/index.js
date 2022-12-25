ad560fdc9304296ce916f1c772ce3ac85bf29570
ihor <mr.ihor.lyzak@gmail.com>
2022-12-25 21:31:23 +0200

    updated with scss mixins and variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'pages/app/App';
-
-import './index.scss';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
