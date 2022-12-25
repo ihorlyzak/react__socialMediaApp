d01600efdf957ef040ec0ef069a1174711a5359c
ihor <mr.ihor.lyzak@gmail.com>
2022-12-25 21:56:11 +0200

    implemented Register page ui

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -1,10 +1,11 @@
 import { Login } from 'pages/login/Login';
+import { Register } from 'pages/register/Register';
 import './App.scss';
 
 export const App = () => {
   return (
     <div className='App'>
-      <Login />
+      <Register />
     </div>
   );
 };
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './register.scss';
+
+export const Register = () => {
+    return (
+        <div className='register'>
+            <div className='card'>
+                <div className='left'>
+                    <h1>React stuff</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem laboriosam quisquam doloribus vero officia earum architecto magnam, voluptates sed. Doloremque eaque ad nihil, tempore excepturi animi similique officiis quam nulla.</p>
+                    <span>Do you have an account?</span>
+                    <button>Login</button>
+                </div>
+
+                <div className='right'>
+                    <h1>Register</h1>
+                    <form>
+                        <input type='text' placeholder='Username' />
+                        <input type='email' placeholder='Email' />
+                        <input type='password' placeholder='Password' />
+                        <input type='text' placeholder='Name' />
+                        <button>Register</button>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    );
+};
